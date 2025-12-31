f8eca9b9fd55ac4e47ac3a3e62d61140ddccebd0
abanhitler <abinms1659@gmail.com>
2025-12-31 09:47:25 +0700

    revisi

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print ("hello world!")
+class Person:
+    def __init__(self, name, interests, location):
+        self.name =  name
+        self.interests = interests
+        self.location = location
+
+    def introduce(self):
+        items = ", ".join(self.interests)
+        return f"Hello, my name is {self.name}. I enjoy {items}. I am currently based in {self.location}."
+    
+me = Person(
+    name="Abin Mohammad Syafrial",
+    interests=["volleyball", "football", "coffee"],
+    location="Jember, Indonesia"
+)
+
+print(me.introduce())
